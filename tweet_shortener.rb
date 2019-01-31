6423f97b60350b1bf5b301be0ed97120931166d9
Ammar Ali <funnyguy118@gmail.com>
2019-01-31 18:11:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+
+
+
+
+def word_substituter (tweet_string)
+  dictionary = {
+    "hello" =>"hi",
+    "to, two, too" => '2',
+    'for, four' => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+  
+  tweet_array = tweet_string.split(" ")
+  
+  tweet_array.map do |word|
+      dictionary.map do |key, value|
+          if key == word
+            word = value
+          end
+      end
+  end
+  
+  tweet_array.join(" ")
+end
