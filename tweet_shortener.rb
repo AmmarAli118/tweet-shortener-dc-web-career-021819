4662701e6f03dea39ffebb3b0022e235f0517514
Ammar Ali <funnyguy118@gmail.com>
2019-01-31 18:22:11 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,19 +1,20 @@
 # Write your code here.
-
-
-
-
+  
+  
+def dictionary (word)
+  dictionary = {
+  "hello" =>"hi",
+  "to, two, too" => '2',
+  'for, four' => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@",
+  "and" => "&"
+  }
+end 
 
 def word_substituter (tweet_string)
-  dictionary = {
-    "hello" =>"hi",
-    "to, two, too" => '2',
-    'for, four' => '4',
-    'be' => 'b',
-    'you' => 'u',
-    "at" => "@",
-    "and" => "&"
-  }
+
   
   tweet_array = tweet_string.split(" ")
   
